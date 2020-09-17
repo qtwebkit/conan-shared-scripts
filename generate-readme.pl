ec119109980a50a9a262d6805775b13e9fad6b31
Konstantin Tokarev <annulen@yandex.ru>
2020-09-17 22:05:48 +0300

    generate-readme.pl: ensure build.cfg exists

--- generate-readme.pl
+++ generate-readme.pl
@@ -16,7 +16,7 @@ sub write_file {
     close $fh;
 }
 
-
+-e 'build.cfg' or die "build.cfg is missing";
 my $config = Config::Tiny->read('build.cfg');
 write_file('README.md', $config->{package});
 write_file('LICENSE', {});
